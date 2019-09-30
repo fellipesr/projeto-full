6764e1accbde3010d5527a538a1b5f50d0fc6e9e
fellipesr <55992886+fellipesr@users.noreply.github.com>
2019-09-30 14:49:56 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyC_3_aKXng-GKhYnAOiDwaPZoo69u6aYiA",
-    authDomain: "full-engenharia.firebaseapp.com",
-    databaseURL: "https://full-engenharia.firebaseio.com",
-    projectId: "full-engenharia",
-    storageBucket: "",
-    messagingSenderId: "95092100061",
-    appId: "1:95092100061:web:52be048bb9334bea"
+    apiKey: xxx
   }
 };
 
